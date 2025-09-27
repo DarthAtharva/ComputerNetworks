9fcac6b25d1b19804adb0fb8cf70c38390711875
Atharva Bhawalkar <atharvabhawalkar147@gmail.com>
2025-09-27 19:09:12 +0530

    Created UDP Server

---
+++ UDP_Server/Server.java
@@ -0,0 +1,35 @@
+package UDP_Server;
+
+import java.io.IOException;
+import java.net.DatagramSocket;
+import java.net.DatagramPacket;
+
+public class Server {
+
+    public static void main(String[] darth){
+
+        try(DatagramSocket serverSocket = new DatagramSocket(2053)){
+
+            System.out.println("SERVER up and running");
+
+            while(true){
+
+                final byte[] buffer = new byte[512];
+                final DatagramPacket datagram = new DatagramPacket(buffer, buffer.length);
+                serverSocket.receive(datagram);
+                System.out.println("Received data from Client: " + new String(datagram.getData(), 0, datagram.getLength()));
+
+                final byte[] bufferResponse = "GeneralKenobi!!!".getBytes();
+                final DatagramPacket datagramResponse = new DatagramPacket(bufferResponse, bufferResponse.length);
+                serverSocket.send(datagramResponse);
+                System.out.println("Response sent");
+
+            }
+
+        }catch(IOException e){
+
+            System.out.println("IO Exception: " + e.getMessage());
+
+        }
+    }
+}
